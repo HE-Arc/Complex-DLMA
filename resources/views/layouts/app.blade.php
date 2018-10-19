2bd72a5376445de4380d412a41a9843d1f121f4c
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2018-10-19 18:23:33 +0200

    Included the app.js script to make Bootstrap work correctly

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,9 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+        <!-- Scripts -->
+        <script src="{{asset('js/app.js')}}"></script>
     </head>
     <body>
         @include("inc.navbar")
