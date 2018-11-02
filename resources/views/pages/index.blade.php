6a069a1f2d61fcdd0961c2fbc176760986d3c24e
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2018-11-02 08:36:13 +0100

    Home page responsive design

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,9 +7,9 @@
         </div>
     </div>
     <div class="cd_choices-main-container row text-center">
-        <div class="col-12 col-lg-6 p-0 p-lg-3">
-            <div class="cd_choice-inner-container cd_choice1 col-12 p-3 shadow">
-                <div class="col-12 cd_choice-text-container p-5">
+        <div class="col-12 col-lg-6 p-3 p-lg-3">
+            <div class="cd_choice-inner-container cd_choice1 col-12 shadow">
+                <div class="col-12 cd_choice-text-container p-3 p-lg-5">
                     <div class="cd_choice-perc col-12">
                         55%
                     </div>
@@ -22,9 +22,9 @@
                 </div>
             </div>
         </div>
-        <div class="col-12 col-lg-6 p-0 p-lg-3">
-            <div class="cd_choice-inner-container cd_choice2 col-12 p-3 shadow">
-                <div class="col-12 cd_choice-text-container p-5">
+        <div class="col-12 col-lg-6 p-3 p-lg-3">
+            <div class="cd_choice-inner-container cd_choice2 col-12 shadow">
+                <div class="col-12 cd_choice-text-container p-3 p-lg-5">
                     <div class="cd_choice-perc col-12">
                         45%
                     </div>
