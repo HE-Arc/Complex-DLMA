38ff9a93f798539d52098c8b4792af5bbabaefed
Ludovic Herbelin <ludovic.herbelin@he-arc.ch>
2018-12-11 14:30:20 +0100

    Cleaned axios comments

--- public/js/app.js
+++ public/js/app.js
@@ -96,18 +96,6 @@ try {
     __webpack_require__(15);
 } catch (e) {}
 
-/*window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-let token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}*/
-
 /***/ }),
 
 /***/ 13:
