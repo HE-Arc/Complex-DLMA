f80c477d17ac93d34f41948574f291b42245f879
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2018-12-12 13:26:08 +0100

    Minor logo correction

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
 
-    <link rel="shortcut icon" sizes="114x114" href="img/logo_square.png">
+    <link rel="shortcut icon" sizes="114x114" href="{{ asset('img/logo_square.png') }}">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
